07a20d2d4c9f7b0eb709ca90f8e3f19ca504e93d
marutkin <marutkin.dmitriy@gmail.com>
2018-10-15 18:25:20 +0300

    fixed routing

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'source')))
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('*', function (req, res) {
+    res.redirect('/');
+  }).get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
